4edecd456677aa06acfe5771064e1b6a7e32a8cd
Mike Nguyen <mikenguyen017@gmail.com>
2022-10-27 23:26:26 +0100

    Nodes now highlight upon selection

--- src/main/java/com/mikenyugen/linearcodes/controllers/ParityNodeController.java
+++ src/main/java/com/mikenyugen/linearcodes/controllers/ParityNodeController.java
@@ -1,6 +1,7 @@
 package com.mikenyugen.linearcodes.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
@@ -8,13 +9,14 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Responsible for querying elements in the parity node FXML.
  */
-public class ParityNodeController implements Initializable {
+public class ParityNodeController implements Initializable, FxmlLoaderRetriever {
   /**
    * The number of message nodes present on the canvas.
    */
@@ -23,6 +25,8 @@ public class ParityNodeController implements Initializable {
    * List of characters representing Labels for the parity node.
    */
   static final String CHAR_LIST = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  ToolBarController toolBarController;
 
   @FXML
   Label squareLabel;
@@ -37,15 +41,33 @@ public class ParityNodeController implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    try {
+      loadControllers();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
     numNodes++;
     setupStyles();
   }
+
+  private void loadControllers() throws IOException {
+    FXMLLoader toolbarLoader = retrieveLoader("/com/mikenyugen/linearcodes/ToolBar.fxml");
+    toolBarController = toolbarLoader.getController();
+  }
+
 
   private void setupStyles() {
     circleLabel.setText("P" + numNodes);
     circleLabel.setStyle("-fx-font-size: 18");
     squareLabel.setText(String.valueOf(CHAR_LIST.charAt(numNodes)));
     squareLabel.setStyle("-fx-font-size: 25");
+  }
+
+  @FXML
+  private void mouseClickedHandler() {
+    if (toolBarController.selectIsSelected()) {
+      square.getStyleClass().add("nodeSelected");
+    }
   }
 
   @FXML
