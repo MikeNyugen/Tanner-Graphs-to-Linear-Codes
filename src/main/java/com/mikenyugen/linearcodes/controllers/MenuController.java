e3ae93c4d0a079342c420972e4e395ecf701dc6f
Mike Nguyen <mikenguyen017@gmail.com>
2022-10-27 03:38:29 +0100

    Spinners working

--- src/main/java/com/mikenyugen/linearcodes/controllers/MenuController.java
+++ src/main/java/com/mikenyugen/linearcodes/controllers/MenuController.java
@@ -4,20 +4,33 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Spinner;
 
+import java.awt.*;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
+/**
+ * Controller responsible for side menu interactions.
+ */
 public class MenuController implements Initializable {
   @FXML
-  private Spinner<Integer> sourceSpinner;
+  SpinnerController spinnerController;
 
-  @FXML
-  void temp() {
-    sourceSpinner.setDisable(true);
-  }
-
+  /**
+   * Defines methods that run on initial load of the scene.
+   *
+   * @param location
+   * The location used to resolve relative paths for the root object, or
+   * {@code null} if the location is not known.
+   *
+   * @param resources
+   * The resources used to localize the root object, or {@code null} if
+   * the root object was not localized.
+   */
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 
   }
+
+
 }
