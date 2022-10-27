31aa46a3071d8efbd331fc08090ee72bc53a43b2
Mike Nguyen <mikenguyen017@gmail.com>
2022-10-27 19:34:13 +0100

    Added FxmlComponent interface to remove duplicate code

--- src/main/java/com/mikenyugen/linearcodes/model/MessageNode.java
+++ src/main/java/com/mikenyugen/linearcodes/model/MessageNode.java
@@ -1,9 +1,5 @@
 package com.mikenyugen.linearcodes.model;
 
-import com.mikenyugen.linearcodes.controllers.MessageNodeController;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -11,36 +7,22 @@ import java.io.IOException;
 import static jfxtras.labs.util.event.MouseControlUtil.makeDraggable;
 
 /**
- * Responsible for displaying the message node correctly.
+ * JavaFX component representing a message node.
  * <p>
  * New components must extend from an existing JavaFX component to be displayed.
+ * <p>
+ * The 'FxmlComponent' interface defines a default method to load the FXML file.
  */
-public class MessageNode extends AnchorPane {
-  @FXML
-  MessageNodeController messageNodeController;
-
-  public MessageNode() throws IOException {
-    super();
-    fxmlSetup();
-    makeDraggable(this);
-  }
+public class MessageNode extends AnchorPane implements FxmlComponent {
 
   /**
-   * Sets up the FXML for the message node.
+   * Loads FXML on initialisation and makes the node draggable.
    *
    * @throws IOException if the FXML cannot be loaded
    */
-  private void fxmlSetup() throws IOException {
-    try {
-      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-          "/com/mikenyugen/linearcodes/MessageNode.fxml"));
-      Node node = fxmlLoader.load();
-      this.getChildren().add(node);
-    } catch (IOException e) {
-      throw new IOException("FXML loader failure");
-    }
+  public MessageNode() throws IOException {
+    fxmlSetup(this, "/com/mikenyugen/linearcodes/MessageNode.fxml");
+    makeDraggable(this); // Makes the node draggable, method imported from JFxtras.labs
   }
-
-
 
 }
--- src/main/java/com/mikenyugen/linearcodes/model/ParityNode.java
+++ src/main/java/com/mikenyugen/linearcodes/model/ParityNode.java
@@ -1,9 +1,5 @@
 package com.mikenyugen.linearcodes.model;
 
-import com.mikenyugen.linearcodes.controllers.ParityNodeController;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -11,35 +7,24 @@ import java.io.IOException;
 import static jfxtras.labs.util.event.MouseControlUtil.makeDraggable;
 
 /**
- * Responsible for displaying the parity node correctly.
+ * JavaFX component representing a parity node.
  * <p>
  * New components must extend from an existing JavaFX component to be displayed.
+ * <p>
+ * The 'FxmlComponent' interface defines a default method to load the FXML file.
  */
-public class ParityNode extends AnchorPane {
-  @FXML
-  ParityNodeController parityNodeController;
-
-  public ParityNode() throws IOException {
-    super();
-    fxmlSetup();
-    makeDraggable(this);
-  }
+public class ParityNode extends AnchorPane implements FxmlComponent {
 
   /**
-   * Sets up the FXML for the parity node.
+   * Loads FXML on initialisation and makes the node draggable.
    *
    * @throws IOException if the FXML cannot be loaded
    */
-  private void fxmlSetup() throws IOException {
-    try {
-      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-          "/com/mikenyugen/linearcodes/ParityNode.fxml"));
-      Node node = fxmlLoader.load();
-      this.getChildren().add(node);
-    } catch (IOException e) {
-      throw new IOException("FXML loader failure");
-    }
+  public ParityNode() throws IOException {
+    fxmlSetup(this, "/com/mikenyugen/linearcodes/ParityNode.fxml");
+    makeDraggable(this);
   }
+
 }
 
 
